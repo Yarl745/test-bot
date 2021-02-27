037abb8300cf9ca5a883e890f4db0120573c64d7
Yarikslav <kopailo2002@gmail.com>
2021-02-27 21:24:57 +0200

    Upgrade for show_activity command.

--- handlers/users/admin/show_activity.py
+++ handlers/users/admin/show_activity.py
@@ -1,11 +1,11 @@
 import logging
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
 
 from data.config import ADMINS
-from loader import dp, db
+from loader import dp, db, activity_saver
 
 
 @dp.message_handler(Command("show_activity"), chat_id=ADMINS, state="*")
@@ -15,7 +15,7 @@ async def show_activity(message: Message):
     day_hour_clicks = await db.get_24_hour_clicks()
 
     # Example one output string --> 11:00-12:00  —>  324
-    info = f"\n".join(
+    info_activity = f"\n".join(
         [
             f"{(hour_clicks['saving_date']-timedelta(hours=1)).strftime('%H:%M')}-"
             f"{hour_clicks['saving_date'].strftime('%H:%M')}  —>"
@@ -24,7 +24,12 @@ async def show_activity(message: Message):
         ]
     )
 
+    now = datetime.now()
+    сurrent_activity = f"Сейчас: {(now-timedelta(minutes=now.minute)).strftime('%H:%M')}-" \
+                       f"{now.strftime('%H:%M')}  —>  {activity_saver.get_count_hour_clicks()}"
+
     await message.answer(
         f"Информация по активным пользователям за 24 часа:\n\n"
-        f"{info}"
+        f"{info_activity}\n\n"
+        f"{сurrent_activity}"
     )
